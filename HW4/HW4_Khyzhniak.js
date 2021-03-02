3411bcdda8ea63f762a11df5f9a53fc23cdf293d
Dmytro <blackdeer12@gmail.com>
2021-03-02 17:39:34 +0200

    Added deepClone function. Minor improvements

--- HW4/HW4_Khyzhniak.js
+++ HW4/HW4_Khyzhniak.js
@@ -23,10 +23,10 @@ function task1() {
     user.introduceYourself = function() {
       setTimeout(function() {
         console.log('%c --- ADDING setTimeout() => loosing a context --- ', 'color: red; font-weight: bold; font-size: 1em;');
-        console.log(`Hey, I'm ${this.name}`); // 'Hey, I`m *blank space*' | IT LOOSES A CONTEXT! setTimeout PASSES window AS this INSIDE A FUNCTION
+        console.log(`Hey, I'm ${this.name}`);
       }, 0);
     }
-    user.introduceYourself(); 
+    user.introduceYourself(); // 'Hey, I`m *blank space*' | IT LOOSES A CONTEXT! setTimeout PASSES window AS this INSIDE A FUNCTION
   
     // LET`S FIX IT WITH AN ARROW FUNCTION
     user.introduceYourself = function() {
@@ -37,10 +37,10 @@ function task1() {
         // AND IT DOESN`T ACCEPT window AS this FROM setTimeout()
         // SO SHE TAKES IT FROM THE closure
         // this === user
-        console.log(`Hey, I'm ${this.name}`); // 'Hey, I`m Dmytro!' | GREAT, WE HAVE FIXED IT!
+        console.log(`Hey, I'm ${this.name}`);
       }, 0);
     }
-    user.introduceYourself();
+    user.introduceYourself(); // 'Hey, I`m Dmytro!' | GREAT, WE HAVE FIXED IT!
   }
 
   function example1_1_0() {
@@ -55,12 +55,12 @@ function task1() {
         console.log('%c --- USING this INSIDE OF CALLBACK => loosing a context --- ', 'color: red; font-weight: bold; font-size: 1em;');
         this.age++;
         this.friends.forEach(function (friend) {
-          console.log(`Invited ${friend} to ${this.name}'s birthday!`); // Invited Dmytro to *blank space*'s birthday! IT LOOSES A CONTEXT!
+          console.log(`Invited ${friend} to ${this.name}'s birthday!`);
           // this === undefined INSIDE OF callback
         });
       }
     }
-    user.celebrateBirthday();
+    user.celebrateBirthday(); // Invited Dmytro to *blank space*'s birthday! IT LOOSES A CONTEXT!
 
     // TO FIX this WE CAN PASS OUR user INSIDE OF forEach AS SECOND ARGUMENT
     user.celebrateBirthdayWithFriends = function() {
@@ -120,20 +120,17 @@ function task2() {
   }
 
   console.log('%c --- SPREAD OPERATOR --- ', 'color: green; font-weight: bold; font-size: 1em;');
-  const child1 = {...root, id: '#1', name: 'child 1', children: []};
-  root.children.push(child1);
+  root.children.push({...root, id: '#1', name: 'child 1', children: []});
 
-  console.log(root);
-  console.log(child1);
+  console.log(root.children[0]);
 
   console.log('%c --- Object.assign() --- ', 'color: green; font-weight: bold; font-size: 1em;');
-  const child2 = Object.assign({}, child1);
+  const child2 = Object.assign({}, root.children[0]);
   child2.name = 'child 2';
   child2.id = '#2';
   child2.children = [];
-  child1.children.push(child2);
+  root.children[0].children.push(child2);
 
-  console.log(child1);
   console.log(child2);
 
   console.log('%c --- for in --- ', 'color: green; font-weight: bold; font-size: 1em;');
@@ -146,17 +143,15 @@ function task2() {
   child3.children = [];
   child2.children.push(child3);
 
-  console.log(child2);
   console.log(child3);
 
   console.log('%c --- JSON.stringify() - JSON.parse() --- ', 'color: green; font-weight: bold; font-size: 1em;');
-  let child4 = JSON.stringify(child3);
-  child4 = JSON.parse(child4);
+  let child4 = JSON.parse(JSON.stringify(child3))
   child4.name = 'child 4';
   child4.id = '#4';
+  child4.children = [];
   child3.children.push(child4);
 
-  console.log(child3);
   console.log(child4);
 
   console.log('%c --- COPYIED AS A LINK --- ', 'color: green; font-weight: bold; font-size: 1em;');
@@ -164,35 +159,46 @@ function task2() {
   child5.name += '-5';
   child5.id += '-5';
 
-  console.log(child4);
   console.log(child5);
 
   console.log('%c --- COPYIED WITH NESTED PROPERTIES --- ', 'color: green; font-weight: bold; font-size: 1em;');
-  const child6 = { 
-    name: 'child 6',
-    id: '#6',
-    prop1: 'value 1',
-    prop2: 'value 2',
-    children: deepClone(root)
-  };
 
-  function deepClone(obj) {
-    const objectsArray = [];
+  /*
+   I don't want to reinvent the wheel.
 
-    function next(obj) {
-      objectsArray.push(obj);
-      for (let each of obj.children) {
-        next(each);
-      }
-    } next(obj);
+   For example these functions already exists in other libraries:
+      lodash - cloneDeep
+      AngularJS - angular.copy
+      jQuery - jQuery.extend()
 
-    return objectsArray;
+    So I have found and optimized a similar function for my needs.
+  */
+
+  const child6 = deepClone(root);
+  child6.name = 'child 6';
+  child6.id = '#6';
+
+
+  function deepClone(from, to) {
+    if (from == null || typeof from != "object") return from;
+    if (from.constructor != Object && from.constructor != Array) return from;
+    if (from.constructor == Date || from.constructor == RegExp || from.constructor == Function ||
+      from.constructor == String || from.constructor == Number || from.constructor == Boolean)
+      return new from.constructor(from);
+
+    to = to || new from.constructor();
+
+    for (var name in from) {
+      to[name] = typeof to[name] == "undefined" ? deepClone(from[name], null) : to[name];
+    }
+
+    return to;
   }
+
   child5.children.push(child6);
 
-  console.log(child5);
   console.log(child6);
-
+  console.log(child6.children[0].children[0] === root.children[0].children[0]);// false | CLONED WITH NESTED PROPERTIES
   task3(root);
 } task2();
 
@@ -209,6 +215,8 @@ function task3(obj) {
     const map = new Map();
 
     function makeFlat(obj) {
+      console.log(obj.id);
+
       if (!map.has(obj.id)) {
         map.set(obj.id, obj.name)
       }
