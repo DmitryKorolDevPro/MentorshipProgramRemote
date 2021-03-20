4b1224212e3424215bcde9c071e3154cac060845
Lana Trupkina <svtrupkina@gmail.com>
2021-03-20 20:00:52 +0200

    for review: fixed mistakes

--- HW6(Async-RegExp)/HW6_Trupkina.js
+++ HW6(Async-RegExp)/HW6_Trupkina.js
@@ -1,45 +1,54 @@
 
-function validEmail() {
-    //Set the default value
-    let email = prompt('Type correctly email please', `example@gmail.com`);
-    const regexp_email = /^([\w\.+-]{3,})+@[A-z0-9-]{3,10}\.[A-z]{2,6}$/;//we can't use \w in domen name and sub name
-    if (email === null) return undefined;//i catch cancellation
-    else if (regexp_email.test(email) === true) return alert('This is your Email:\n' + email);
-    return validEmail();
+let email = ['example@gmail.com', //true
+    'EXAMPLE@MAIL.RU',            //true
+    'EXam_p.l-e@gmaiL.Com',       //true
+    'EXam_p.l35e@gmaiL.Com',      //true
+    '@gmaiL.Com',                 //false
+    'gss@Yandex.mojong',          //true
+    'gs@Yandex.mojong',           //false 
+    'example @gmail.com',         //false
+    'example@356.ru',             //true 
+    'example@356.333',            //false
+    'example@Flora-mix.ru',       //true
+    'exa23@44m.p-le@Flora-mix.ru' //false - for all ()!@#$%^&*=
+];
+console.log(validEmail(email));
+function validEmail(mail) {
+    const regexp_email = /^([\w\.+-]{3,})+@[A-z0-9-]{3,10}\.[A-z]{2,6}$/;//we can't use \w in domen name and sub name because '_' included
+    for (const iterator of mail) {
+        //console.log(`${iterator}: ${regexp_email.test(iterator) === true}`);//can be write shorter
+        if (regexp_email.test(iterator) === true) {
+            console.log(`${iterator}: true`);
+        }
+        else {
+            console.log(`${iterator}: false`);
+        };
+    }
 }
 
+let password = ['aaaaaaaaA#',     //true
+    'AAAAAAA22a#',                //true
+    'парОль3%',                   //false
+    'passwOrd3%',                 //true
+    'AAAAAAAAAA#',                //false
+    'aA#',                        //false
+    '2222222222A#',               //false
+    '2222222222a#',               //false
+    '22222222222',                //false
+    '2222222222#',                //false
+    '############'                //false
+];
+console.log(validPassword(password));
 function validPassword(pass) {
-    //Set the default value with Math.random() if user doesn't want to come up with password
-    let password = prompt('Type your password please', Math.floor(Math.random() * Math.floor(999999)) + 'Add!t');
+    /* only Latin and numbers, and a special character are allowed " ?,!, @, -, +, = ".
+     Large and small letters must be present, the password is at least 8 characters long */
     const regexp_pass = /^.*(?=.{8,})(?=.+[A-Z])(?=.+[a-z])(?=.*[!@#$%^&*?+=-]).*$/;
-    if (password === null) return undefined;//i catch cancellation
-    else if (regexp_pass.test(password) === true) return alert('This is your password:\n' + password);
-    return validPassword();
+    for (const iterator of pass) {
+        if (regexp_pass.test(iterator) === true) {
+            console.log(`${iterator}: true`);
+        }
+        else {
+            console.log(`${iterator}: false`);
+        }
+    }
 }
-
-console.log(validPassword());
-//testing password:
-// aaaaaaaaA# //true
-// AAAAAAA22a# //true
-// AAAAAAAAAA# //false
-// aA# //false
-// 2222222222A#//false
-// 2222222222a#//false
-// 22222222222 //false
-// 2222222222# //false
-// ############ //false
-
-console.log(validEmail());
-//testing email:
-//example@gmail.com //true
-//EXAMPLE@MAIL.RU //true
-//EXam_p.l-e@gmaiL.Com //true
-//EXam_p.l35e@gmaiL.Com //true
-//@gmaiL.Com //false
-//gss@Yandex.mojong //true
-//gs@Yandex.mojong //false 
-//example @gmail.com //false
-//example@356.ru //true 
-//example@356.333 //false
-//example@Flora-mix.ru //true
-//exa23@44m.p-le@Flora-mix.ru //false - for all ()!@#$%^&*=
