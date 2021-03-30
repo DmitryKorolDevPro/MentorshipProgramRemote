a71d295cb546a45bcaa78f4df51f198b87b9efce
Lana Trupkina <svtrupkina@gmail.com>
2021-03-30 13:38:29 +0300

    Division into MVC

--- HW8(MVC)/js/javascript.js
+++ HW8(MVC)/js/javascript.js
@@ -1,20 +1,33 @@
+//Model
+//function to load todo if list is found in local storage.
+function loadTodo() {
+    if (localStorage.getItem('todoList')) {
+        list.innerHTML = localStorage.getItem('todoList');
+    }
+}
+//save todolist state so user can access it later
+function saveElement() {
+    localStorage.setItem('todoList', list.innerHTML);
+    alert('Your TODO Saved!');
+}
 
+//View
 var list = document.querySelector('ul');
-
-list.addEventListener('click', function (ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    } else if (ev.target.tagName === 'SPAN') {
-        var div = ev.target.parentNode;
+list.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked');
+    } else if (event.target.tagName === 'SPAN') {
+        var div = event.target.parentNode;
         div.remove();
     }
 }, false);
 
-function newElement() {
+function addElement() {
     var li = document.createElement('li');
     var inputValue = document.getElementById('toDoEl').value;
-    var t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    var text = document.createTextNode(inputValue);
+    li.appendChild(text);
+
     if (inputValue == '') {
         alert('Type your note');
     } else {
@@ -29,16 +42,18 @@ function newElement() {
     li.appendChild(span);
 }
 
-//function to load todo if list is found in local storage.
-function loadTodo() {
-    if (localStorage.getItem('todoList')) {
-        list.innerHTML = localStorage.getItem('todoList');
-    }
-}
-//save todolist state so user can access it later
-function saveElement() {
-    localStorage.setItem('todoList', list.innerHTML);
-
-}
-
-loadTodo();
+//Controller
+var addButton = document.querySelector(".addBtn");
+var saveButton = document.querySelector(".saveBtn");
+//onclick newElement()
+addButton.addEventListener('click', () => {
+    addElement();
+});
+//onclick saveElement()
+saveButton.addEventListener('click', () => {
+    saveElement();
+});
+//onPageLoad loadTodo()
+document.addEventListener("DOMContentLoaded", () => {
+    loadTodo();
+});
