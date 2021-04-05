bc0bf282ec71cdba0a8de3512f6718f7abc52b2e
Lana Trupkina <svtrupkina@gmail.com>
2021-04-05 17:28:16 +0300

    tests will be ready soon

--- HW8(MVC)/unit.test.js
+++ HW8(MVC)/unit.test.js
@@ -1,21 +1,50 @@
-const { addButton, saveButton } = require('./javascript');
-const { describe, test, expect } = require('@jest/globals');
 
-describe('testing...', () => {
-    test('Testing true value addButton: ', () => {
+import { describe, test, expect, jest } from '@jest/globals';
+import { model } from './model.js';
 
-        expect(addButton('text')).toBe('text');
+describe('Testing model...', () => {
+    const todo = 'Hello Mr World!';
+    test('Testing saveElement... ', () => {
+        const setItem = jest.spyOn(Storage.prototype, 'setItem');
+        model.saveElement(todo);
 
+        expect(setItem).toHaveBeenCalledWith('todoList', todo);
+    });
+    test('Testing loadTodo... ', () => {
+        const getItem = jest.spyOn(Storage.prototype, 'getItem');
+
+        const notify = jest.spyOn(Storage.prototype, 'notify');
+
+        model.loadTodo();
+
+        expect(notify).toHaveBeenCalledWith('todoList', getItem);
     });
 });
-// describe('Testing Controller... ', () => {
-//     describe('On click button Add: should add into list ', () => {
+//                this.savedTodo = new Observer();
+ //               loadTodo() {
+ //                   if (localStorage.getItem('todoList')) {
+ //                       this.savedTodo.notify(localStorage.getItem('todoList'));
+ //                   }
+//                saveElement(todo) {
+//                   localStorage.setItem('todoList', todo);
+//                 }
+
+// it("calls getItem", () => {
+    // mockGetItem = jest.fn().mockReturnValue('{"id": "1"}');
+    // const res = funcToTest();
+    // expect(mockGetItem.mock.calls.length).toBe(1);
+    // sessionStorage.getItem("abc");
+
+    // expect(sessionStorage.getItem).toHaveBeenCalledWith("abc");
+
+//  // https://github.com/facebook/jest/issues/6858
+
+
+// describe('Testing controller...', () => {
+//     test('Testing  ', () => {
 
 //     });
-// });
-
-// describe('Testing Model... ', () => {
-//     describe(' ', () => {
+//     test('Testing ', () => {
 
 //     });
 // });
